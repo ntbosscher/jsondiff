5e2f0eefd4ccd8b5ae31879b1807dd3dd674ce74
Nathan Bosscher <nate.bosscher@gmail.com>
2020-11-25 14:40:32 -0500

    add diff formatting to allow for multiple ways to represent the diff - add support for primitive array diffing

--- main.go
+++ main.go
@@ -7,6 +7,41 @@ import (
 // Diff compares oldValue with newValue and returns a json tree of
 // the changed values.
 func Diff(oldValue interface{}, newValue interface{}) (json.RawMessage, error) {
+	return DiffFormat(oldValue, newValue, DefaultFormat)
+}
+
+func DefaultFormat(oldValue interface{}, newValue interface{}) (outputValue interface{}) {
+	return newValue
+}
+
+func NewValueFormat(oldValue interface{}, newValue interface{}) (outputValue interface{}) {
+	return newValue
+}
+
+func OldValueFormat(oldValue interface{}, newValue interface{}) (outputValue interface{}) {
+	return oldValue
+}
+
+func BothValuesAsMapFormat(oldValue interface{}, newValue interface{}) (outputValue interface{}) {
+	return map[string]interface{}{
+		"Old": oldValue,
+		"New": newValue,
+	}
+}
+
+func DiffOldNew(oldValue interface{}, newValue interface{}) (json.RawMessage, error) {
+	return DiffFormat(oldValue, newValue, BothValuesAsMapFormat)
+}
+
+// Formatter controls how to represent the diff in the output json message
+// e.g. to show only the newValue, this func would return newValue
+// e.g. to show only the oldValue, this func would return oldValue
+// e.g. to show a {old: <v>, new: v}, this func would return map[string]interface{}{ "old": oldValue, "new": newValue }
+//
+// oldValue & newValue will always be non-struct types
+type Formatter func(oldValue interface{}, newValue interface{}) (outputValue interface{})
+
+func DiffFormat(oldValue interface{}, newValue interface{}, formatter Formatter) (json.RawMessage, error) {
 	jsonOld, err := json.Marshal(oldValue)
 	if err != nil {
 		return nil, err
@@ -28,7 +63,7 @@ func Diff(oldValue interface{}, newValue interface{}) (json.RawMessage, error) {
 	}
 
 	diff := map[string]interface{}{}
-	calculateDiff(diff, oldMap, newMap)
+	calculateDiff(diff, oldMap, newMap, formatter)
 
 	return json.Marshal(diff)
 }
@@ -39,6 +74,7 @@ func calculateDiff(
 	diffResult map[string]interface{},
 	oldMap map[string]interface{},
 	newMap map[string]interface{},
+	formatter Formatter,
 ) {
 
 	// iterate over keys
@@ -51,14 +87,14 @@ func calculateDiff(
 
 		// one is a map, the other is not, must be a change
 		if oldIsMap != newIsMap {
-			diffResult[k] = newProp
+			diffResult[k] = formatter(oldProp, newProp)
 			continue
 		}
 
 		// both are maps, check subkeys for changes
 		if oldIsMap && newIsMap {
 			subResult := map[string]interface{}{}
-			calculateDiff(subResult, mpOld, mpNew)
+			calculateDiff(subResult, mpOld, mpNew, formatter)
 
 			// has subkey differences, add to diff
 			if len(subResult) > 0 {
@@ -68,9 +104,42 @@ func calculateDiff(
 			continue
 		}
 
+		arrOld, oldIsArr := oldProp.([]interface{})
+		arrNew, newIsArr := newProp.([]interface{})
+
+		// one is an array, the other is not, must be a change
+		if oldIsArr != newIsArr {
+			diffResult[k] = formatter(oldProp, newProp)
+			continue
+		}
+
+		// both are arrays, let's dive in
+		if oldIsArr && newIsArr {
+			// lengths don't match, must be a change
+			if len(arrOld) != len(arrNew) {
+				diffResult[k] = formatter(oldProp, newProp)
+				continue
+			}
+
+			different := false
+
+			for i := 0; i < len(arrOld); i++ {
+				if arrOld[i] != arrNew[i] {
+					different = true
+					break
+				}
+			}
+
+			if different {
+				diffResult[k] = formatter(oldProp, newProp)
+			}
+
+			continue
+		}
+
 		// regular value comparison (non-map values)
 		if newProp != oldProp {
-			diffResult[k] = newProp
+			diffResult[k] = formatter(oldProp, newProp)
 		}
 	}
 }
--- main_test.go
+++ main_test.go
@@ -5,34 +5,90 @@ import (
 	"testing"
 )
 
-func Example_SimpleDiff() {
-	type Entity struct {
-		ID       int
-		Name     string
-		Relation *Entity
-	}
+type ExampleEntity struct {
+	ID       int
+	Name     string
+	List []int
+	Relation *ExampleEntity
+}
 
-	a := &Entity{
+func Example_Diff() {
+	A := &ExampleEntity{
 		ID:   1,
 		Name: "John",
-		Relation: &Entity{
+		List: []int{3,2,1},
+		Relation: &ExampleEntity{
 			ID:   3,
 			Name: "Ken",
 		},
 	}
 
-	b := &Entity{
+	B := &ExampleEntity{
 		ID:   2,
 		Name: "John",
-		Relation: &Entity{
+		List: []int{1,2,3},
+		Relation: &ExampleEntity{
 			ID:   2,
 			Name: "asdf",
 		},
 	}
 
-	diff, _ := Diff(a, b)
+
+	diff, _ := Diff(A, B)
 	fmt.Println(string(diff))
-	// Output: {"ID":2,"Relation":{"ID":2,"Name":"asdf"}}
+	// Output: {"ID":2,"List":[1,2,3],"Relation":{"ID":2,"Name":"asdf"}}
+}
+
+func Example_DiffOldNew() {
+	A := &ExampleEntity{
+		ID:   1,
+		Name: "John",
+		List: []int{3,2,1},
+		Relation: &ExampleEntity{
+			ID:   3,
+			Name: "Ken",
+		},
+	}
+
+	B := &ExampleEntity{
+		ID:   2,
+		Name: "John",
+		List: []int{1,2,3},
+		Relation: &ExampleEntity{
+			ID:   2,
+			Name: "asdf",
+		},
+	}
+
+	diff, _ := DiffOldNew(A, B)
+	fmt.Println(string(diff))
+	// Output: {"ID":{"New":2,"Old":1},"List":{"New":[1,2,3],"Old":[3,2,1]},"Relation":{"ID":{"New":2,"Old":3},"Name":{"New":"asdf","Old":"Ken"}}}
+}
+
+func Example_DiffFormat() {
+	A := &ExampleEntity{
+		ID:   1,
+		Name: "John",
+		List: []int{3,2,1},
+		Relation: &ExampleEntity{
+			ID:   3,
+			Name: "Ken",
+		},
+	}
+
+	B := &ExampleEntity{
+		ID:   2,
+		Name: "John",
+		List: []int{1,2,3},
+		Relation: &ExampleEntity{
+			ID:   2,
+			Name: "asdf",
+		},
+	}
+
+	diff, _ := DiffFormat(A, B, OldValueFormat)
+	fmt.Println(string(diff))
+	// Output: {"ID":1,"List":[3,2,1],"Relation":{"ID":3,"Name":"Ken"}}
 }
 
 type Entity struct {
@@ -126,6 +182,92 @@ func TestNestedDiffNestedChange(t *testing.T) {
 	}
 
 	if string(diff) != `{"Relation":{"ID":12}}` {
+		t.Error("invalid diff")
+	}
+}
+
+func TestFormatBoth(t *testing.T) {
+	a := &Entity{
+		ID:   2,
+		Name: "asdf",
+		Relation: &Entity{
+			ID:       2,
+			Name:     "asdf",
+			Relation: nil,
+		},
+	}
+
+	b := &Entity{
+		ID:   2,
+		Name: "asdf",
+		Relation: &Entity{
+			ID:       12,
+			Name:     "asdf",
+			Relation: nil,
+		},
+	}
+
+	diff, err := DiffFormat(a, b, BothValuesAsMapFormat)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(diff) != `{"Relation":{"ID":{"New":12,"Old":2}}}` {
+		t.Error(string(diff))
+		t.Error("invalid diff")
+	}
+}
+
+func TestArrayNoChange(t *testing.T) {
+	type EntityWithArray struct {
+		ID int
+		List []int
+	}
+
+	a := &EntityWithArray{
+		ID:   2,
+		List: []int{1,2},
+	}
+
+	b := &EntityWithArray{
+		ID:   2,
+		List: []int{1,2},
+	}
+
+	diff, err := DiffFormat(a, b, BothValuesAsMapFormat)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(diff) != `{}` {
+		t.Error(string(diff))
+		t.Error("invalid diff")
+	}
+}
+
+func TestArrayChanged(t *testing.T) {
+	type EntityWithArray struct {
+		ID int
+		List []int
+	}
+
+	a := &EntityWithArray{
+		ID:   2,
+		List: []int{1,2},
+	}
+
+	b := &EntityWithArray{
+		ID:   2,
+		List: []int{1,2,3},
+	}
+
+	diff, err := DiffFormat(a, b, BothValuesAsMapFormat)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(diff) != `{"List":{"New":[1,2,3],"Old":[1,2]}}` {
+		t.Error(string(diff))
 		t.Error("invalid diff")
 	}
 }
